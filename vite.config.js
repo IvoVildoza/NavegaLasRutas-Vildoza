035e2ccf6d7868d7acfdada5fd7d973b3848dbe4
IvoVildoza <ivo_nofear@hotmail.com>
2026-02-01 05:03:33 -0300

    Config GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/NavegaLasRutas+Vildoza/',
+  base: '/NavegaLasRutas-Vildoza/',
   plugins: [react()],
 })
